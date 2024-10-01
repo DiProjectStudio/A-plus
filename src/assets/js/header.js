bad408834c53c3c897637d5cd5680c755ffd237e
IlyaDOC <ilya22@inbox.ru>
2024-10-01 12:23:46 +0300

    fixed header navbar styles

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const headerMenuToggle = document.getElementById("headerMenuToggle");
     const headerNav = document.querySelector('.header__nav');
     const closeBtn = document.getElementById("closeBtn");
-    const headerNavItems = document.querySelectorAll('.header__nav a')
+    const headerNavItems = document.querySelectorAll('.header__nav a');
+
     if (headerMenuToggle) {
         headerMenuToggle.addEventListener("click", () => {
             headerNav.classList.toggle("active");
@@ -21,29 +22,13 @@ document.addEventListener("DOMContentLoaded", () => {
     if (closeBtn) {
         closeBtn.addEventListener("click", () => {
             if (headerNav.classList.contains("active")) {
-                headerNav.style.animation = 'slideUp .5s'; // Устанавливаем анимацию
-                console.log('click');
-                // headerNav.classList.remove("active");
-                // Удаляем класс active после завершения анимации
-                headerNav.addEventListener('animationend', () => {
-                    console.log('click1')
-                    headerNav.classList.remove("active");
-                    headerNav.style.animation = ''; // Сбрасываем стиль анимации
-                }, { once: true }); // Удаляем обработчик после первого срабатывания
+                headerNav.classList.remove("active");
             }
         })
     }
 
-    $('.slider').on('click', function() {
+    $('.slider').on('click', function () {
         $(this).toggleClass('slider-closed');
     });
 
-    // document.addEventListener('scroll', function() {
-    //     const header = document.querySelector('.header');
-    //     if (window.scrollY > 0) {
-    //         header.style.transform = 'translateY(10px)';
-    //     } else {
-    //         header.style.transform = 'translateY(0)';
-    //     }
-    // });
 })
